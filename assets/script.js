1f75039be6677c3c4b0dfdf69ce3baed5f792dac
JakeD <43915365+jakeD9@users.noreply.github.com>
2019-04-12 15:51:54 -0500

    other pages

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $("#arrow-group").on("click", function() {
+        const portfolio = document.getElementById("portfolio")
+        portfolio.scrollIntoView();
+    });
+
+})
+
+
+
+
+
